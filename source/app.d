7f5b86b332c9989b94cf030091dff29d00f495b6
Seiji Emery <seijiemery@gmail.com>
2020-06-27 14:16:37 -0700

    got test anim working

--- source/app.d
+++ source/app.d
@@ -5,6 +5,7 @@ import std.exception: enforce;
 import std.format: format;
 import std.c.stdlib: malloc, free, realloc;
 import sprites;
+import std.stdio;
 
 //extern(C) void* nkAlloc (nk_handle handle, void* ptr, size_t size) nothrow @nogc {
 //	return ptr ? malloc(size) : realloc(ptr, size);
@@ -44,11 +45,22 @@ void main() {
 	InitWindow(800, 600, "Hello, Raylib-D!");
 	Sprites Sprites;
 	Sprites.load();
-	while (!WindowShouldClose())
-	{
+
+	auto sprite = Sprites.Player.Roll;
+	double SPEED = 10;
+	//size_t i = 0;
+
+	while (!WindowShouldClose()) {
 		 BeginDrawing();
 		 ClearBackground(RAYWHITE);
 		 DrawText("Hello, World!", 400, 300, 28, BLACK);
+
+		 //DrawTextureQuad(sprite.frames[0], Vector2(0, 0), Vector2(400, 300), Rectangle(32,32), WHITE);
+		 size_t i = cast(size_t)(GetTime() * sprite.animationSpeed * SPEED) % sprite.frames.length;
+		 //writefln("%s %s", GetTime(), cast(size_t)GetTime());
+		 DrawTexture(sprite.frames[i], 400, 300, WHITE);
+		 //i = (i + 1) % sprite.frames.length;
+
 		 EndDrawing();
 	}
 	CloseWindow();
